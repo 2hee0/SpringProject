1ef1eb4bee3ade82e2d42e4b23ffe3c088305675
yumyumsom <woosw13@gmail.com>
2023-10-20 14:30:43 +0900

    [ver 1.2] 회원가입 경로 및 css 수정

--- src/main/java/com/onehundredmillion/library/dto/JoinForm.java
+++ src/main/java/com/onehundredmillion/library/dto/JoinForm.java
@@ -9,11 +9,13 @@ public class JoinForm {
     private String userid;
     private String name;
     private String password;
-    private String re_password;
-    private String RRN;
+    private String rePassword;
+    private String rrn; //주민번호
+    private String phoneNo;
     private String addr;
     private String addr_detail;
     private String addr_etc;
+    private String extraAddr;
     private String zipcode;
 
 
