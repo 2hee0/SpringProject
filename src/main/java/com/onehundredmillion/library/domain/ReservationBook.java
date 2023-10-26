907f4eccaf6f8b7e629773123399b2dd86b1bfb2
yumyumsom <woosw13@gmail.com>
2023-10-26 17:37:13 +0900

    [ver 4.4] 예약하기 기능 수정

---
+++ src/main/java/com/onehundredmillion/library/domain/ReservationBook.java
@@ -0,0 +1,41 @@
+package com.onehundredmillion.library.domain;
+
+import com.onehundredmillion.library.exception.NotEnoughStockException;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.ColumnDefault;
+
+
+@Entity
+@Getter
+@Setter
+public class ReservationBook {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "RESERVATION_BOOK", nullable = false)
+    private Long id;
+
+    @ColumnDefault("1")
+    private int count;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "BOOK_ID")
+    private Book book;
+
+
+    @ManyToOne
+    @JoinColumn(name = "RESERVATION_ID")
+    private Reservation reservation;
+
+    public static ReservationBook createReservationBook(Book book, int count) throws NotEnoughStockException {
+        ReservationBook reservationBook = new ReservationBook();
+        reservationBook.setBook(book);
+
+        return reservationBook;
+    }
+
+    public void cancel() {
+        reservation.cancel();
+    }
+}
