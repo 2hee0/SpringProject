0ebd00f59abcfefbc6401380fc192a5442d189f6
yumyumsom <woosw13@gmail.com>
2023-10-27 10:09:02 +0900

    [ver 5.1] 관리자 책 등록 기본 기능

--- src/main/java/com/onehundredmillion/library/controller/BookController.java
+++ src/main/java/com/onehundredmillion/library/controller/BookController.java
@@ -7,6 +7,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
@@ -15,22 +17,24 @@ import java.util.List;
 @RequiredArgsConstructor
 public class BookController {
     private final BookService bookService;
-  /*책 추가 관리자용(추후에,,)
-  @GetMapping(value = "/items/new")
+
+    //  책 추가 관리자용
+    @GetMapping(value = "/admin/new")
     public String createForm(Model model) {
         model.addAttribute("form", new BookForm());
-        return "items/createItemForm";
+        return "admin/createItemForm";
     }
-    @PostMapping(value = "/items/new")
+
+    @PostMapping(value = "/admin/new")
     public String create(BookForm form) {
         Book book = new Book();
-        book.setName(form.getName());
+        book.setName(form.getTitle());
         book.setStockQuantity(form.getStockQuantity());
-        book.setAuthor(form.getAuthor());
+        book.setAuthor(form.getAuthors());
         book.setIsbn(form.getIsbn());
-        bookService.savdBooks(book);
+        bookService.saveBooks(book);
         return "redirect:/items";
-    }*/
+    }
 
 
     @GetMapping(value = "/books")
@@ -39,21 +43,41 @@ public class BookController {
         model.addAttribute("books", books);
         return "book/bookList";
     }
-    
+
     @GetMapping(value = "/books/reserve")
     public String reserve(Model model) {
         Book book = bookService.reserve();
         model.addAttribute("book", book);
         return "book/reserve";
     }
-    
+
     @GetMapping(value = "/books/rent")
     public String rent(Model model) {
         Book book = bookService.rent();
         model.addAttribute("book", book);
         return "book/rent";
     }
-    
 
+
+    @GetMapping(value = "/book/{bookId}/edit")
+    public String updateBookForm(@PathVariable("bookId") Long bookId, Model
+            model) {
+        Book item = (Book) bookService.findOne(bookId);
+        BookForm bookForm = new BookForm();
+        bookForm.setId(item.getId());
+        bookForm.setTitle(item.getName());
+        bookForm.setStockQuantity(item.getStockQuantity());
+        bookForm.setAuthors(item.getAuthor());
+        bookForm.setIsbn(item.getIsbn());
+        model.addAttribute("bookForm", bookForm);
+        return "admin/updateItemForm";
+    }
+
+    @PostMapping(value = "/book/{bookId}/edit")
+    public String updateItem(@PathVariable Long bookId, @ModelAttribute("bookForm")
+    BookForm bookForm) {
+        bookService.updateBook(bookId, bookForm.getTitle(), bookForm.getStockQuantity());
+        return "redirect:/booklist";
+    }
 
 }
--- src/main/java/com/onehundredmillion/library/dto/BookForm.java
+++ src/main/java/com/onehundredmillion/library/dto/BookForm.java
@@ -3,16 +3,21 @@ package com.onehundredmillion.library.dto;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.time.chrono.ChronoLocalDateTime;
+
 @Getter @Setter
 public class BookForm {
 
     private Long id;
 
-    private String name;
-    private String author;
+    private String title;
+    private String authors;
     private String isbn;
     private String contents;
-    private String company;
+    private String publisher;
+//    private Datetime datetime;
+    private String thumbnail;
     private int stockQuantity;
+
 
 }
