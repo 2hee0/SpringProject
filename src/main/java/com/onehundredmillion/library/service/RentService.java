a898dd63c89f1d42c1b898034fd76aede7e9f0be
yumyumsom <woosw13@gmail.com>
2023-11-01 12:13:07 +0900

    [ver 9.2] 헤더 css 추가

--- src/main/java/com/onehundredmillion/library/service/RentService.java
+++ src/main/java/com/onehundredmillion/library/service/RentService.java
@@ -23,27 +23,27 @@ public class RentService {
     public List<Rent> findAll(Member member) {
         return rentRepository.findAll(member.getId(), BookStatus.RENT);
     }
-    
-    // 대여하기 
-   @Transactional
-	public Long rent(Member member, Long bookId) throws NotEnoughStockException {
-		Member saveMember = memberRepository.findOne(member.getId());
-		Book book = bookRepository.findOne(bookId);
-		RentBook rentBook = RentBook.createRentBook(book, 1);
 
-		if(rentRepository.rentCheck(saveMember.getId(), book.getId()) == 0) {
-			// 대여정보 생성
-			Rent rent = Rent.createRent(saveMember, rentBook);
-			// 대여정보 저장
-			rentRepository.rent(rent);
-			return (long)rent.getId();
-		}
-		else {
-			return (long) 0;
-		}
-	}
+    // 대여하기
+    @Transactional
+    public Long rent(Member member, Long bookId) throws NotEnoughStockException {
+        Member saveMember = memberRepository.findOne(member.getId());
+        Book book = bookRepository.findOne(bookId);
+        RentBook rentBook = RentBook.createRentBook(book, 1);
 
-   // 반납하기
+        if(rentRepository.rentCheck(saveMember.getId(), book.getId()) == 0) {
+            // 대여정보 생성
+            Rent rent = Rent.createRent(saveMember, rentBook);
+            // 대여정보 저장
+            rentRepository.rent(rent);
+            return (long)rent.getId();
+        }
+        else {
+            return (long) 0;
+        }
+    }
+
+    // 반납하기
     @Transactional
     public void returnBook(Long rentId) {
         //주문 엔티티 조회
